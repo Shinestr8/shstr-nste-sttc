a722793deb1c9ff318e37fb9ebd350be11d91059
Corentin <shinedownizstr8@gmail.com>
2022-04-14 15:42:59 +0200

    remove useless api route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {fork, spawn} = require('child_process');
+const {spawn} = require('child_process');
 const app = express()
 const port = 5000
 
@@ -8,28 +8,6 @@ app.use(express.json())
 function replaceAll(string, search, replace) {
     return string.split(search).join(replace);
 }
-
-app.get('/', (req, res) => {
- 
- var dataToSend;
- // spawn new child process to call the python script
- const python = spawn('python', ['./python/function.py', 1, 3]);
- python.stderr.pipe(process.stderr);
- // collect data from script
- python.stdout.on('data', function (data) {
-  console.log('Pipe data from python script ...');
-  dataToSend = data.toString();
- });
- // in close event we are sure that stream from child process is closed
- python.on('close', (code) => {
- console.log(`child process close all stdio with code ${code}`);
- // send data to browser
- console.log(dataToSend)
- result = replaceAll(dataToSend, "'", '"')
- res.json(JSON.parse(result))
- });
- 
-})
 
 app.get('/predict', (req, res) => {
     const url = req.query.url;
@@ -51,5 +29,4 @@ app.get('/predict', (req, res) => {
 
    })
 
-app.listen(port, () => console.log(`Example app listening on port 
-${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))

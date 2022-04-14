332141066c13861b0f1fa9700954a920c3883c32
Corentin <shinedownizstr8@gmail.com>
2022-04-14 16:51:29 +0200

    simple css design

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import {useState} from "react";
+import './App.css';
 
 function App() {
 
   const [youtubeURL, setYoutubeURL] = useState("");
-  const [data, setData] = useState(null);
+  const [data, setData] = useState({"guess":[{"name":"blues","count":3},{"name":"classical","count":8},{"name":"country","count":6},{"name":"disco","count":4},{"name":"hiphop","count":7},{"name":"jazz","count":1},{"name":"pop","count":151},{"name":"reggae","count":4},{"name":"rock","count":1}],"higherGuess":"pop","total":185,"message":"success"});
+
   const [loading, setLoading] = useState(false)
   function handleValueChange(e){
     setYoutubeURL(e.target.value);
@@ -21,22 +23,33 @@ function App() {
 
   return (
     <div id="page">
+
+    
+    <div id="content">
       <form>
         <input placeholder="youtube link" type="text" value={youtubeURL} onChange={handleValueChange}/>
         <button type="submit" onClick={submitLink}>Submit</button>
       </form>
       {/* {youtubeURL && youtubeURL} */}
       {loading && (<div>Processing your song...</div>)}
-      {data && (
-        <h2>Main prediction: {data.higherGuess}</h2>
+      {data &&(
+        <div id="prediction">
+          <h2>Main prediction: {data.higherGuess}</h2>
+
+          <ul>
+
+          
+          {data.guess.map(function(genre, index){
+            return(
+              <li key={`genre-${index}`}>
+                {genre.name} {Math.round(10000*genre.count/data.total)/100}%
+              </li>
+            )
+          })}
+          </ul>
+        </div>
       )}
-      {data && data.guess.map(function(genre, index){
-        return(
-          <div key={`genre-${index}`}>
-            {genre.name} {Math.round(10000*genre.count/data.total)/100}%
-          </div>
-        )
-      })}
+    </div>
     </div>
   );
 }

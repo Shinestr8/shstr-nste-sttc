d46511e11b980ba40c234e107fbb549f3cc2b6c4
Corentin <shinedownizstr8@gmail.com>
2022-04-14 15:40:13 +0200

    clean code

--- server/python/predict.py
+++ server/python/predict.py
@@ -3,66 +3,46 @@ import numpy as np
 import tensorflow as tf
 import yt_dlp
 import sys 
-# import shutil
 import os
 
-
-#model name
 modelName = 'GTZANPretrainedCNN.h5'
 genres = ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"]
 genres.sort()
 
+
+#function that removes downloaded file after prediction is over
 def clean(url):
-    video_url = url.split("&")[0]
-    video_id = video_url.split("=")[1]
+    video_id = url.split("=")[1]
     filename = str("tmp/" + video_id + ".m4a")
     os.remove(filename)
 
 
 def downloadSong(url):
-    # orig_ext = ".webm"
-    video_url = url.split("&")[0]
-    video_id = video_url.split("=")[1]
+    video_id = url.split("=")[1] #parse URL and keep video ID, will be used as filename
 
     video_info = yt_dlp.YoutubeDL().extract_info(
-        url = video_url,download=False
+        url = url,download=False
     )
 
+    #ignore videos that are longer than 10min
     if(int(video_info["duration"]) > 600 ):
-        result = {"message": "song is too long"}
-        print(result)
+        output = {"message": "song is too long"}
+        print(output)
         sys.exit(0)
 
     filename = str("tmp/" + video_id + ".m4a")
-    # filename = 'tmp/music'
-
-    # options={
-    #     'format': 'bestaudio/best',
-    #     'postprocessors': [{
-    #         'key': 'FFmpegExtractAudio',
-    #         'preferredcodec': 'mp3',
-    #         'preferredquality': '192'
-    #     }],
-    #     'keepvideo':False,
-    #     'outtmpl':filename + orig_ext,
-    # }
     options={
         'format':'m4a',
         'keepvideo':False,
         'outtmpl':filename,
     }
-
     with yt_dlp.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
-    
-    # print("end func")
-    # return filename
-
 
 #preocess the song to predict
 def processSong(songData):
     songLength = songData.shape[0]
-    chunkSize = 33000
+    chunkSize = 33000 #=1.5sec
     spectrograms = []
     for i in range(0, songLength, chunkSize):
             signal = np.array(songData[i:i+chunkSize])#load a chunk
@@ -74,76 +54,71 @@ def processSong(songData):
     spectrograms = np.array(spectrograms)
     return spectrograms
 
-# def processInChunk():
 
-
+#node childprocess didnt have enough RAM to process a song in one go
+#instead, split the song in chunks of 2min and process one after another
 def predictInChunk(url):
-    video_url = url.split("&")[0]
-    video_id = video_url.split("=")[1]
+    video_id = url.split("=")[1]
     filename = str("tmp/" + video_id + ".m4a")
     signal, sr = librosa.load(filename)
-    chunksize_minutes=2
-    step = int(((chunksize_minutes*60)/1.5)*33000)
-    subdivs = signal.shape[0]//step
-    rest = signal.shape[0]%step
+    chunksize_minutes=2 #split file in 2min parts
+    step = int(((chunksize_minutes*60)/1.5)*33000) #calculate size of a 2min chunk
+    subdivs = signal.shape[0]//step #how many whole 2min chinks
+    rest = signal.shape[0]%step #what remains after division
     
     CNN = tf.keras.models.load_model('python/models2/GTZANPretrainedCNN.h5') #load the model
-    # print(signal.shape[0])
-    # print(subdivs)
-    totalUnique = np.array([], dtype=int)
-    totalCounts = np.array([], dtype=int)
+    totalUnique = np.array([], dtype=int) #array that will store the genres detected
+    totalCounts = np.array([], dtype=int) #array that will store the count of each genre
     for i in range(0, subdivs, 1):
-        
         start = i*step
         end = start + step
         melspec = processSong(signal[start:end])
         prediction = np.argmax(CNN.predict(melspec), axis=-1) #predict the classes
         
+        #add the predicted class to the total
         newUnique, newCounts = np.unique(prediction, return_counts=True)
         totalUnique, idx = np.unique(np.hstack((totalUnique, newUnique)), return_inverse=True)
         totalCounts = np.bincount(idx, np.hstack((totalCounts, newCounts)))
-        # print(newUnique, newCounts)
-        # print(totalUnique, totalCounts)
         del melspec
         del newUnique
         del newCounts
 
+    #only execute this if there is enough data for a chunk
     if(rest > 33000):
         melspec = processSong(signal[subdivs*step:subdivs*step+rest])
         prediction = np.argmax(CNN.predict(melspec), axis=-1) #predict the classes
         del melspec
+        newUnique, newCounts = np.unique(prediction, return_counts=True)
+        totalUnique, idx = np.unique(np.hstack((totalUnique, newUnique)), return_inverse=True)
+        totalCounts = np.bincount(idx, np.hstack((totalCounts, newCounts)))
+        totalCounts = totalCounts.astype(int)
 
-    newUnique, newCounts = np.unique(prediction, return_counts=True)
-    totalUnique, idx = np.unique(np.hstack((totalUnique, newUnique)), return_inverse=True)
-    totalCounts = np.bincount(idx, np.hstack((totalCounts, newCounts)))
-    totalCounts = totalCounts.astype(int)
-    # labels = genres
-    total = 0
+    
     higherGuess = np.argmax(totalCounts)
-    bestGuess = genres[totalUnique[higherGuess]]
+    bestGuess = genres[totalUnique[higherGuess]] #genre with most predictions
     array = []
     for i in range(len(totalUnique)):
         array.append({"name": genres[totalUnique[i]], "count": totalCounts[i]})
 
+    #count total chunks
+    total = 0
     for c in totalCounts:
         total = total + c
 
 
-    result = {
+    output = {
         "guess": array,
         "higherGuess": bestGuess,
         "total": total,
         "message": "success"
     }
 
-    print(result)
-    # del melspec
+    print(output)
     
 def main():
-    ytURL = sys.argv[1]
-    # ytURL = 'https://www.youtube.com/watch?v=uczY1We-Eak'
+    param = sys.argv[1]
+    ytURL = param.split("&")[0]
     downloadSong(ytURL)
-    
     predictInChunk(ytURL)  
     clean(ytURL)
     

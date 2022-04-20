7da57e9075249f9a72bc3e91f4a6abe757c02a81
Corentin <shinedownizstr8@gmail.com>
2022-04-20 15:22:26 +0200

    add preventdefault and url check

--- client/src/components/Predict.jsx
+++ client/src/components/Predict.jsx
@@ -7,16 +7,20 @@ import { GenreRadarChart } from "./GenreRadarChart";
 export function Predict(){
     const [youtubeURL, setYoutubeURL] = useState("");
     const [data, setData] = useState({"guess":[{"name":"hiphop","count":68},{"name":"pop","count":45},{"name":"classical","count":10},{"name":"country","count":4},{"name":"disco","count":4},{"name":"jazz","count":2},{"name":"rock","count":2},{"name":"reggae","count":1}],"higherGuess":"hiphop","higherCount":68,"total":136,"message":"success","rawData":[1,1,1,1,1,1,1,1,1,4,4,7,3,7,4,7,4,7,7,7,4,7,7,7,7,7,7,4,4,4,4,4,4,4,7,4,4,7,4,7,7,7,7,7,7,7,7,4,7,4,4,4,4,7,4,7,7,4,7,7,4,7,4,7,5,2,5,2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,8,7,2,7,3,7,7,3,9,7,7,7,7,7,7,7,7,7,7,4,7,2,4,4,4,3,4,4,4,4,9,4,4,4,1]});
-
+    // const [data, setData] = useState(null);
     const [loading, setLoading] = useState(false)
     function handleValueChange(e){
         setYoutubeURL(e.target.value);
     }
-
+    
     async function submitLink(e){
+        e.preventDefault();
+        if(!youtubeURL.includes("watch?v=")){
+            setData({message: "invalid URL"});
+            return
+        }
         setData(null);
         setLoading(true);
-        e.preventDefault();
         const response = await fetch("/predict?url="+youtubeURL);
         const newData = await response.json();
         setData(newData);
@@ -42,7 +46,7 @@ export function Predict(){
                     <div>{data.message}</div>
                 )}
                 {data && data.message==="success" &&(
-            <div id="prediction">
+                <div id="prediction">
                 
                         <div className="top-left">
                             <ul>

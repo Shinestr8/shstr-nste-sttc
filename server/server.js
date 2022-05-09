d19be0ed3ff0104beb9f2a34fd76b891597fe779
Corentin <shinedownizstr8@gmail.com>
2022-05-09 16:26:22 +0200

    use new routes

--- server/server.js
+++ server/server.js
@@ -1,50 +1,23 @@
 const express = require('express')
-const {spawn} = require('child_process');
-const route = require('./routes/route');
-const app = express()
-const port = 5000
 
-app.use(express.json());
-app.use('/api/feedback', route);
+
+const feedbackRoute = require('./routes/feedback');
+const aiRoute = require('./routes/ai')
+
+const app = express()
+const port = process.env.PORT || 5000
 
 require('./initDB')();
 
+app.use(express.json());
+app.use('/api/feedback', feedbackRoute);
+app.use('/ai', aiRoute)
 
-
-function replaceAll(string, search, replace) {
-    return string.split(search).join(replace);
-}
-
-function isValidLink(url){
-    if(url.includes("watch?v=")){
-        return true
-    }
-    return false
-}   
 
 app.post('/yes', function(req, res){
     console.log(req.body);
     res.json("non")
 })
 
-app.get('/predict', (req, res) => {
-    const url = req.query.url;
-    if(!isValidLink(url)){
-        res.json({"message": "This is not a valid youtube URL"})
-        return
-    }
-    var result;
-    const python = spawn('python', ['./python/predict.py', url]);
-    python.stdout.on('data', function(data){
-        rawString = data.toString();
-    })
-
-    python.on('close', function(code){
-        console.log("predict close with code" + code);
-        result = replaceAll(rawString, "'", '"')
-        res.json(JSON.parse(result))
-    })
-
-   })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

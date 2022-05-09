fc40623ff77166915e2ce2630b70c92a984a0840
Corentin <shinedownizstr8@gmail.com>
2022-05-09 12:33:51 +0200

    added prediction table with styling

---
+++ client/src/components/PredictionTable.jsx
@@ -0,0 +1,46 @@
+
+import { useState } from "react"
+
+export function PredictionTable(){
+
+
+    const [data, setData] = useState([
+        {
+            status: 'fail',
+            predictedLabel: 'hiphop',
+            trueLabel: 'pop',
+            youtubeURL: 'https://www.youtube.com/watch?v=9KW7hojmoaI'
+        },
+        {
+            status: 'success',
+            predictedLabel: 'metal',
+            trueLabel: 'metal',
+            youtubeURL: 'https://www.youtube.com/watch?v=9KW7hojmoaI'
+        }
+    ])
+
+    return(
+        <div id="table-container">
+            <table id="prediction-table">
+                <thead>
+                    <tr>
+                        <th className="first-col">Predicted label</th>
+                        <th>True label</th>
+                        <th className="last-col">Song URL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data && data.map(function(line, index){
+                        return(
+                            <tr className={`prediction-${line.status}`}>
+                                <td>{line.predictedLabel}</td>
+                                <td>{line.trueLabel}</td>
+                                <td><a href={line.youtubeURL}>{line.youtubeURL}</a></td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}

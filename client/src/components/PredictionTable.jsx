0aa6a50bd86e349afa4d2e9d7b4b17989c1e3b7e
Corentin <shinedownizstr8@gmail.com>
2022-05-12 15:47:33 +0200

    fetch more data when reaching bottom of the page

--- client/src/components/PredictionTable.jsx
+++ client/src/components/PredictionTable.jsx
@@ -1,21 +1,72 @@
 
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
+import useWindowDimensions from "./tool/windowDimensions";
+
+
 
 export function PredictionTable(){
 
-    useEffect(()=>{
-        async function fetchData(){
-          const res = await fetch('/api/feedback/');
-          const data = await res.json();
-          setData(data);
-        }
-        fetchData()
-      }, [])
+    const ref = useRef(null);
+
+    const {height, width} = useWindowDimensions()
+
+    
+    
+
 
     const [data, setData] = useState(null);
+    const [count, setCount] = useState(0);
+
+
+    // useEffect(() => {
+    //     console.log("height", ref.current.offsetHeight);
+    //     console.log("total", height)
+    //     if(ref.current.offsetHeight < height){
+    //         setCount(c=>c+1)
+    //     }
+    //   }, []);
+
+    useEffect(()=>{
+        async function fetchData(){
+          const response = await fetch(`/api/feedback?page=${count}`);
+          const result = await response.json();
+          if(count === 0){
+            setData(result);
+          }
+          else {
+            setData((data) => [...data, ...result]);
+          }
+          if(ref.current.offsetHeight < height && result.length > 0){
+              setCount(c=>c+1)
+          }
+        }
+        fetchData()
+        
+      }, [count, height])
+
+
+      function handleScroll(e){
+        const el = e.target.documentElement;
+        const bottom = el.scrollHeight - el.scrollTop - 1 < el.clientHeight;
+        if (bottom) {
+          setCount(c => c+1)
+         }
+      }
+
+    useEffect(()=>{
+        window.addEventListener('scroll',handleScroll);
+        return(function(){
+            window.removeEventListener("scroll", handleScroll)
+        })
+    }, [])
+    
+      
 
     return(
-        <div id="table-container">
+        <div 
+            id="table-container" 
+            ref={ref}
+        >
             <table id="prediction-table">
                 <thead>
                     <tr>
@@ -30,8 +81,9 @@ export function PredictionTable(){
                             <tr 
                                 key={index}     
                                 className={line.success ? 'prediction-success' : 'prediction-fail'}
+                                id={index === data.length-1 ? 'last' : null}
                             >
-                                <td>{line.predictedLabel}</td>
+                                <td>{index} {line.predictedLabel}</td>
                                 <td>{line.trueLabel}</td>
                                 <td>
                                     <a 

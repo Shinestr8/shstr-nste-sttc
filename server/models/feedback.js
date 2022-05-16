8b20fa93eae691fdb4d8652d4d22804403c56d8c
Corentin <shinedownizstr8@gmail.com>
2022-05-16 13:37:41 +0200

    change feedback model to store guess data

--- server/models/feedback.js
+++ server/models/feedback.js
@@ -2,7 +2,42 @@ const mongoose = require('mongoose');
 
 const collectionName = 'feedback';
 
+const GuessSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    count:{
+        type:Number,
+        required: true
+    }
+}, {_id: false})
+
+
 const feedbackSchema = new mongoose.Schema({
+    data:{
+        guess: [GuessSchema],
+        higherGuess:{
+            type: String,
+            required: true
+        },
+        higherCount:{
+            type: Number,
+            required: true
+        },
+        total:{
+            type: Number,
+            required: true
+        },
+        message:{
+            type: String,
+            required: true
+        },
+        rawData: {
+            type:[Number],
+            required: true
+        }
+    },
     predictedLabel:{
         type: String,
         required: true

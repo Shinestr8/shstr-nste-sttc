014309fe5a39586fe7041dac4a657a41c87d2d6d
Corentin <shinedownizstr8@gmail.com>
2022-05-16 15:24:08 +0200

    new prediction component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Page } from "./components/Page";
 import { About } from './components/About/About';
 import { Test } from './components/Test';
 import { PredictionTable } from './components/Prediction/PredictionTable';
+import { Prediction } from './components/Prediction/Prediction';
 // import {Test} from "./components/Test"
 function App() {
 
@@ -15,7 +16,7 @@ function App() {
           <Route path="/" element={<Predict/>}/>
           <Route path="about" element={<About/>}/>
           <Route path="predictions" element={<PredictionTable/>}/>
-          <Route path="prediction/:id" element={<Test/>}/>
+          <Route path="prediction/:id" element={<Prediction/>}/>
         </Route>
         <Route path="test" element={<Test><div>Hello</div><div>World</div></Test>}/>
       </Routes>
--- client/src/components/Prediction/Prediction.jsx
+++ client/src/components/Prediction/Prediction.jsx
@@ -1,3 +1,10 @@
+import { useParams } from "react-router-dom";
+
 export function Prediction(){
-    
+    const {id} = useParams();
+    return(
+        <div>
+            {id}
+        </div>
+    )
 }
